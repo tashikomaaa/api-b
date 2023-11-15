a2b50841df31ee69f1859db8defa3afc5709eeb5
aldwin <aldwin@mytroopers.com>
2023-11-15 20:22:31 +0100

    remove bloquing condition

--- src/api/validators/user.validator.js
+++ src/api/validators/user.validator.js
@@ -5,9 +5,9 @@ export function validateRegister(body) {
     email: Joi.string().email().min(3).required(),
     password: Joi.string().min(6).max(20).required(),
     name: Joi.string().min(3).max(24).required(),
-    language: Joi.string().valid('tr', 'en').required(),
-    timezone: Joi.number().required(),
-    deviceId: Joi.string().min(4).required()
+    // language: Joi.string().valid('fr', 'en').required(),
+    // timezone: Joi.number().required(),
+    // deviceId: Joi.string().min(4).required()
   });
   return schema.validate(body);
 }
